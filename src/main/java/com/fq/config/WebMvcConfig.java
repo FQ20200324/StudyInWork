55e20ff3722d94096493d3144a58d50140dd4bcf
fq2025 <fuqiao@deepleaper.com5>
2025-09-30 16:03:35 +0800

    1.3 添加跨域访问

--- src/main/java/com/fq/config/WebMvcConfig.java
+++ src/main/java/com/fq/config/WebMvcConfig.java
@@ -2,21 +2,36 @@ package com.fq.config;
 
 import com.fq.config.interceptor.TraceIdInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     TraceIdInterceptor traceIdInterceptor;
 
     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //addMapping 添加可跨域的请求地址
+        registry.addMapping("/**")
+                //设置跨域 域名权限 规定由某一个指定的域名+端口能访问跨域项目
+                .allowedOrigins("*")
+                //是否开启cookie跨域
+                .allowCredentials(false)
+                //规定能够跨域访问的方法类型
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+
+                .exposedHeaders("traceId")
+                //预检请求存活时间 在此期间不再次发送预检请求
+                .maxAge(3600);
+    }
+
+    @Override
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
-        //确保父类默认注册的拦截器（如Spring内置的HandlerInterceptor）被保留
-        WebMvcConfigurer.super.addInterceptors(registry);
         //配置链路追踪traceId拦截器
         registry.addInterceptor(traceIdInterceptor).addPathPatterns("/**");
     }
--- src/main/java/com/fq/controller/TestController.java
+++ src/main/java/com/fq/controller/TestController.java
@@ -10,15 +10,24 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class TestController {
 
-    @PostMapping ("/Hello")
-    public HelloRsp Hello(@RequestBody HelloReq request){
-        if ("1".equals(request.getType())){
+    @PostMapping("/Hello")
+    public HelloRsp Hello(@RequestBody HelloReq request) {
+        if ("1".equals(request.getType())) {
             HelloRsp helloRsp = new HelloRsp();
             log.info("This should show traceId");
             helloRsp.setPrt("Hello World!");
             return helloRsp;
         }
         return new HelloRsp();
+    }
+
+    @GetMapping("/Test")
+    public HelloRsp TestH() {
+        HelloRsp helloRsp = new HelloRsp();
+        log.info("should show traceId");
+        helloRsp.setPrt("Hello World!");
+        return helloRsp;
+
     }
 
 }
