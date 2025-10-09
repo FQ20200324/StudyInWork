6e11c7ca62801734fe58b2e0bf49b8ba8bbfb30f
fq_home <1211131449@qq.com>
2025-10-09 22:46:55 +0800

    mcp_try unfinish

--- src/main/java/com/fq/App.java
+++ src/main/java/com/fq/App.java
@@ -1,11 +1,21 @@
 package com.fq;
 
+import com.fq.service.CourseService;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+    @Bean
+    public List<ToolCallback> getTools(CourseService courseService) {
+        return List.of(ToolCallbacks.from(courseService));
     }
 }
---
+++ src/main/java/com/fq/entity/Course.java
@@ -0,0 +1,4 @@
+package com.fq.entity;
+
+public record Course(String title, String description, String url) {
+}
