d5562a52844c4da6a11437da6d61b87a900199ca
fq2025 <fuqiao@deepleaper.com5>
2025-10-11 18:28:19 +0800

    1.2 mcp 纯java版

--- src/main/java/com/fq/App.java
+++ src/main/java/com/fq/App.java
@@ -1,21 +1,63 @@
 package com.fq;
 
-import com.fq.service.CourseService;
-import org.springframework.ai.support.ToolCallbacks;
-import org.springframework.ai.tool.ToolCallback;
-import org.springframework.boot.SpringApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.server.McpSyncServer;
+import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
+import io.modelcontextprotocol.spec.McpSchema;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-    @Bean
-    public List<ToolCallback> getTools(CourseService courseService) {
-        return List.of(ToolCallbacks.from(courseService));
+
+        // Sync tool specification
+        var schema = """
+                {
+                  "type" : "object",
+                  "id" : "urn:jsonschema:Operation",
+                  "properties" : {
+                    "operation" : {
+                      "type" : "string"
+                    },
+                    "a" : {
+                      "type" : "number"
+                    },
+                    "b" : {
+                      "type" : "number"
+                    }
+                  }
+                }
+                """;
+        var syncToolSpecification = new McpServerFeatures.SyncToolSpecification(
+                new McpSchema.Tool("calculator", "Basic calculator", schema),
+                (exchange, arguments) -> {
+                    // Tool implementation
+                    String result = "calculator call ok";
+                    return new McpSchema.CallToolResult(result, false);
+                }
+        );
+
+        StdioServerTransportProvider transportProvider = new StdioServerTransportProvider(new ObjectMapper());
+        // Create a server with custom configuration
+        McpSyncServer syncServer = McpServer.sync(transportProvider)
+                .serverInfo("my-server", "1.0.0")
+                .capabilities(McpSchema.ServerCapabilities.builder()
+                        .resources(false, true)  // Enable resource support
+                        .tools(true)             // Enable tool support
+                        .prompts(true)           // Enable prompt support
+                        .logging()               // Enable logging support
+                        .completions()           // Enable completions support
+                        .build())
+                .build();
+
+// Register tools, resources, and prompts
+        syncServer.addTool(syncToolSpecification);
+//        syncServer.addResource(syncResourceSpecification);
+//        syncServer.addPrompt(syncPromptSpecification);
+
+// Close the server when done
+//        syncServer.close();
     }
 }
