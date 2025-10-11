ede5ab8b355819290a7f637c9a8b5a3f0628ab92
fq2025 <fuqiao@deepleaper.com5>
2025-10-11 15:39:02 +0800

    1.4 SSE尝试

--- src/main/java/com/fq/controller/TestController.java
+++ src/main/java/com/fq/controller/TestController.java
@@ -3,7 +3,11 @@ package com.fq.controller;
 import com.fq.model.request.HelloReq;
 import com.fq.model.response.HelloRsp;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("/test")
@@ -28,6 +32,34 @@ public class TestController {
         helloRsp.setPrt("Hello World!");
         return helloRsp;
 
+    }
+
+    /**
+     * SSE测试
+     */
+
+    @GetMapping(value = "/stock-price", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter streamStockPrice() {
+        SseEmitter emitter = new SseEmitter();
+        // 模拟生成实时股票价格并推送给客户端
+        Random random = new Random();
+        new Thread(() -> {
+            try {
+                while (true) {
+                    // 生成随机的股票价格
+                    double price = 100 + random.nextDouble() * 10;
+                    // 构造股票价格的消息
+                    String message = String.format("%.2f", price);
+                    // 发送消息给客户端
+                    emitter.send(SseEmitter.event().data(message));
+                    // 休眠1秒钟
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+                emitter.completeWithError(e);
+            }
+        }).start();
+        return emitter;
     }
 
 }
